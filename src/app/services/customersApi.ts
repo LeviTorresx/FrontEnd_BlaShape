36ed11a0b9d6fce268a637d24c7792238ec64978
LeviTorresx <levist42@gmail.com>
2025-10-22 07:39:10 -0500

    [008]:  Add customer APIs with operations to create, edit, and gets customers

---
+++ src/app/services/customersApi.ts
@@ -0,0 +1,45 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { axiosBaseQuery } from "./axiosBaseQuery";
+import { Customer } from "../types/Customer";
+
+interface CustomerResponse {
+  message: string;
+}
+
+export const customerApi = createApi({
+  reducerPath: "customerApi",
+  baseQuery: axiosBaseQuery({
+    baseUrl: "http://localhost:8080/api_BS/customer",
+  }),
+  endpoints: (builder) => ({
+    createCustomer: builder.mutation<CustomerResponse, Customer>({
+      query: (body) => ({
+        url: "/create",
+        method: "POST",
+        data: body,
+      }),
+    }),
+
+    editCustomer: builder.mutation<CustomerResponse, Customer>({
+      query: (body) => ({
+        url: `/edit/${body.customerId}`,
+        method: "PUT",
+        data: body,
+      }),
+    }),
+
+    getCustomers: builder.query<Customer[], void>({
+      query: () => ({
+        url: "/all",
+        method: "GET",
+      }),
+    }),
+
+    getCustomerById: builder.query<Customer, number>({
+        query: (id) => ({
+          url: `get/${id}`,
+          method: "GET",
+        }),
+    })
+  }),
+});
